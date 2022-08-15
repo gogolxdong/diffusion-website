8b2ffb3694f90f4aea880e00793e6a4f13c47c9a
Daisy <15773281560@163.com>
2022-08-16 00:37:59 +0800

    update

--- src/views/Home/component/Square/index.js
+++ src/views/Home/component/Square/index.js
@@ -9,29 +9,30 @@ import logo2 from '@/assets/img//logo2.png';
 const Square = () => {
     return <div className='square-wrap'>
       <div className="square">
-      <img src={truthVolcano}></img>
-      <p>Next Generation Joint Reserve System</p>
-      <span className="word_line"></span>
-      <span className="bottom_line"></span>
-      <img className="bot_logo" src={logo2}></img>
-      <div className="bot_word">
-        <Row>
-          <Col span={6}>
-            <p>23</p>
-            <p className="smile">Factors of attention</p>
-          </Col>
-          <Col span={6}>
-            <p>23</p>
-            <p className="smile">Factors of attention</p>
-          </Col>
-          <Col span={6}>
-            <p>23</p>
-            <p className="smile">Factors of attention</p>
-          </Col>
-        </Row>
+        <img src={truthVolcano} className='truthVolcano'></img>
+        <div className="word-wrap">
+          <p className="title">Next Generation Joint Reserve System</p>
+          <p className="description">Diffusion Metafi is a set of combined reserve system based on Web3</p>
+          <span className="word_line"></span>
+        </div>
+        <img className="bot_logo" src={logo2}></img>
+        <div className="bot_word">
+          <Row>
+            <Col span={6}>
+              <p>23</p>
+              <p className="smile">Factors of attention</p>
+            </Col>
+            <Col span={6}>
+              <p>23</p>
+              <p className="smile">Factors of attention</p>
+            </Col>
+            <Col span={6}>
+              <p>23</p>
+              <p className="smile">Factors of attention</p>
+            </Col>
+          </Row>
+        </div>
       </div>
-      <p>Diffusion Metafi is a set of combined reserve system based on Web3</p>
-  </div>
     </div>
 }
 export default Square
